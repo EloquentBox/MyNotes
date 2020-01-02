5845df03f6243f03b49e99086ed1ab472289661a
Karol Nowosad <drogakrzyzowa@gmail.com>
2020-01-02 23:54:12 +0000

    Home

---
+++ webNinja/home/home.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  @Input() uberninja;
+  @Output() onYell = new EventEmitter();
+  fireYellEvent(e) {
+    this.onYell.emit(e);
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
